9a0b6e344e277343a951ed4a91ad03b04a7fec39
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-09-11 01:52:40 -0300

    Add Perfil / DAO

---
+++ src/fiap/model/VisualizarPerfil.java
@@ -0,0 +1,15 @@
+package fiap.model;
+
+public class VisualizarPerfil {
+	
+	private int idRegistroGeral;
+
+	public int getIdRegistroGeral() {
+		return idRegistroGeral;
+	}
+
+	public void setIdRegistroGeral(int idRegistroGeral) {
+		this.idRegistroGeral = idRegistroGeral;
+	}
+
+}
